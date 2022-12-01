b913ee80433e458c3a08ef214d6c125eb143f1d8
Erolyn <elise.s.brun@gmail.com>
2022-12-01 18:04:57 +0100

    Add files via upload

---
+++ js/hambuger_menu.js
@@ -0,0 +1,9 @@
+
+const button = document.getElementById('buttonmenu');
+const mobile_list = document.getElementById('mobile_list');
+
+button.addEventListener('click', () => {
+    mobile_list.classList.toggle('active');
+})
+
+/// https://www.youtube.com/watch?v=mh36Kts-wX8&ab_channel=CemEygiMedia
